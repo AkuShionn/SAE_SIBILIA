f4f9a23ad59a229fc50498c009a766cb9700224b
Jorge Alves Da Silva <Jorge.Alves-Da-Silva@etu.univ-smb.fr>
2025-06-06 10:45:30 +0200

    modif

--- SAE_SIBILIA/Windows/CreationClient.xaml.cs
+++ SAE_SIBILIA/Windows/CreationClient.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SAE_SIBILIA.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,7 @@ namespace SAE_SIBILIA.Windows
     {
         public CreationClient()
         {
+            this.DataContext = new Client();
             InitializeComponent();
         }
     }
